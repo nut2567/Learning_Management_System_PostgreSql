15eb17fa53c9165803997c3826d87027eae05b75
nut2567 <nutmakchine.n@gmail.com>
2024-11-24 07:01:41 +0700

    should update UI when filters are changed mockAxiosGet.mockResolvedValueOnce

--- src/__tests__/page/page.test.tsx
+++ src/__tests__/page/page.test.tsx
@@ -10,7 +10,62 @@ import axios from "axios";
 
 vi.mock("axios");
 const mockAxiosGet = vi.mocked(axios.get);
-const mockInstructors = user;
+const mockInstructors = [
+  {
+    id: 2,
+    Instructor_Name: "bukky",
+    createdAt: "2024-11-17T20:47:15.262Z",
+    age: 3.5,
+    email: "bukky@home.com",
+    image: "/bukky.jpg",
+    phone: "098-9895888",
+  },
+  {
+    id: 3,
+    Instructor_Name: "deisy",
+    createdAt: "2024-11-17T20:47:15.270Z",
+    age: 1.2,
+    email: "deisy@home.com",
+    image: "/S__7667726_0.jpg",
+    phone: "098-9895888",
+  },
+  {
+    id: 4,
+    Instructor_Name: "John Wick",
+    createdAt: "2024-11-17T20:47:15.274Z",
+    age: 30,
+    email: "John_Wick@gokillyou.com",
+    image: "/nut.jpg",
+    phone: "5",
+  },
+  {
+    id: 5,
+    Instructor_Name: "Jon Snow",
+    createdAt: "2024-11-17T20:47:15.278Z",
+    age: 40,
+    email: "Jon Snow@filllikecool.com",
+    image: "/Jon_Snow.png",
+    phone: "088-8885648",
+  },
+  {
+    id: 1,
+    Instructor_Name: "Nutthawat",
+    createdAt: "2024-11-17T11:26:15.634Z",
+    age: 28,
+    email: "Nutthawat@gmail.com",
+    image: "/20191216_082746.jpg",
+    phone: "098-2691859",
+  },
+  {
+    id: 6,
+    Instructor_Name: "Titan",
+    createdAt: "2024-11-17T20:47:15.289Z",
+    age: 40,
+    email: "Titan@gmail.com",
+    image: "/Titan.jpg",
+    phone: "026-755888",
+  },
+];
 
 describe("Renders list of products should call API with correct parameters", () => {
   beforeEach(() => {
@@ -341,23 +396,22 @@ describe("Renders list of products should call API with correct parameters", () 
         },
       },
       {
-        id: 71,
-        courseTitle: "Full-Stack Software Development19",
-        courseDuration: 100,
-        level: "Intermediate",
-        enrollmentCount: 50,
+        id: 28,
+        courseTitle: "Back-End Development6",
+        courseDuration: 3,
+        level: "Advanced",
+        enrollmentCount: 98,
         createdAt: new Date(),
         status: "Open",
-        image:
-          "https://fastly.picsum.photos/id/60/1920/1200.jpg?hmac=fAMNjl4E_sG_WNUjdU39Kald5QAHQMh-_-TsIbbeDNI",
-        userId: 6,
+        image: "/16_9.png",
+        userId: 3,
         user: {
-          Instructor_Name: "Titan",
-          email: "Titan@gmail.com",
+          Instructor_Name: "deisy",
+          email: "deisy@home.com",
           createdAt: new Date(),
-          age: 40,
-          image: "/Titan.jpg",
-          phone: "026-755888",
+          age: 1.2,
+          image: "/S__7667726_0.jpg",
+          phone: "098-9895888",
         },
       },
     ];
@@ -378,6 +432,9 @@ describe("Renders list of products should call API with correct parameters", () 
     expect(mockAxiosGet).toHaveBeenCalledWith(
       "http://localhost:4000/api/instructor"
     );
+    // ตรวจสอบผลลัพธ์ที่คืนค่า
+    expect(initialProducts).toEqual({ courses: mockProducts, total: 2 });
+    expect(initialinstructor).toEqual(mockInstructors);
 
     render(
       <Home
@@ -411,24 +468,157 @@ describe("Renders list of products should call API with correct parameters", () 
       screen.getByText(/A-Z/i, { selector: "#select-sort option" })
     ).toBeInTheDocument();
 
+    expect(mockAxiosGet).toHaveBeenCalledTimes(2); // หรือจำนวนครั้งที่คาดว่าจะเรียก
     vi.clearAllMocks(); // รีเซ็ต 2 แรกเพื่อนับใหม่
+    // Mock response for new filter
+    mockAxiosGet.mockResolvedValueOnce({
+      data: {
+        courses: [
+          {
+            id: 16,
+            courseTitle: "Front-End Development4",
+            courseDuration: 24,
+            level: "Beginner",
+            enrollmentCount: 58205,
+            createdAt: "2024-11-17T22:09:12.716Z",
+            status: "Open",
+            image: "/16_4.png",
+            userId: 2,
+            user: {
+              Instructor_Name: "bukky",
+              email: "bukky@home.com",
+              createdAt: "2024-11-17T20:47:15.262Z",
+              age: 3.5,
+              image: "/bukky.jpg",
+              phone: "098-9895888",
+            },
+          },
+          {
+            id: 17,
+            courseTitle: "Front-End Development5",
+            courseDuration: 24,
+            level: "Beginner",
+            enrollmentCount: 58205,
+            createdAt: "2024-11-17T22:09:12.721Z",
+            status: "Open",
+            image: "/16_4.png",
+            userId: 2,
+            user: {
+              Instructor_Name: "bukky",
+              email: "bukky@home.com",
+              createdAt: "2024-11-17T20:47:15.262Z",
+              age: 3.5,
+              image: "/bukky.jpg",
+              phone: "098-9895888",
+            },
+          },
+        ],
+        total: 2,
+      },
+    });
     // เปลี่ยนค่า Filter และตรวจสอบว่า UI ตอบสนอง
     // Mock การเปลี่ยนค่าใน Select ของ Sort
     fireEvent.change(sortSelect, { target: { value: "Z-A" } });
     expect(screen.getByDisplayValue("Course title (Z-A)")).toBeInTheDocument();
 
-    // Mock การเปลี่ยนค่าใน Select ของ Level
-    fireEvent.change(levelSelect, { target: { value: "Intermediate" } });
-    expect(screen.getByDisplayValue("Intermediate")).toBeInTheDocument();
+    await waitFor(() => {
+      // ตรวจสอบระดับ
+      const lavel = screen.getAllByText(/Beginner/i, {
+        selector: "div > div > div:nth-child(1) > button:nth-child(1)",
+      });
+      expect(lavel).toHaveLength(2);
 
+      expect(
+        screen.getByText(/Front-End Development4/i, {
+          selector: "div:nth-child(2) > h2",
+        })
+      ).toBeInTheDocument();
+    });
+    mockAxiosGet.mockResolvedValueOnce({
+      data: {
+        courses: [
+          {
+            id: 16,
+            courseTitle: "Front-End Development4",
+            courseDuration: 24,
+            level: "Beginner",
+            enrollmentCount: 58205,
+            createdAt: "2024-11-17T22:09:12.716Z",
+            status: "Open",
+            image: "/16_4.png",
+            userId: 2,
+            user: {
+              Instructor_Name: "bukky",
+              email: "bukky@home.com",
+              createdAt: "2024-11-17T20:47:15.262Z",
+              age: 3.5,
+              image: "/bukky.jpg",
+              phone: "098-9895888",
+            },
+          },
+          {
+            id: 17,
+            courseTitle: "Front-End Development5",
+            courseDuration: 24,
+            level: "Beginner",
+            enrollmentCount: 58205,
+            createdAt: "2024-11-17T22:09:12.721Z",
+            status: "Open",
+            image: "/16_4.png",
+            userId: 2,
+            user: {
+              Instructor_Name: "bukky",
+              email: "bukky@home.com",
+              createdAt: "2024-11-17T20:47:15.262Z",
+              age: 3.5,
+              image: "/bukky.jpg",
+              phone: "098-9895888",
+            },
+          },
+        ],
+        total: 2,
+      },
+    });
+    // Mock การเปลี่ยนค่าใน Select ของ Level
+    fireEvent.change(levelSelect, { target: { value: "Beginner" } });
+    expect(screen.getByDisplayValue("Beginner")).toBeInTheDocument();
+    await waitFor(() => {
+      // ตรวจสอบระดับ
+      const lavel = screen.getAllByText(/Beginner/i, {
+        selector: "div > div > div:nth-child(1) > button:nth-child(1)",
+      });
+      expect(lavel).toHaveLength(2);
+
+      expect(
+        screen.getByText(/Front-End Development4/i, {
+          selector: "div:nth-child(2) > h2",
+        })
+      ).toBeInTheDocument();
+    });
     // Mock การเปลี่ยนค่าใน Select ของ Status
     fireEvent.change(statusSelect, { target: { value: "Open" } });
     expect(screen.getByDisplayValue("Open")).toBeInTheDocument();
 
-    fireEvent.change(instructorSelect, { target: { value: "bukky" } });
-    expect(screen.getByDisplayValue("bukky")).toBeInTheDocument();
+    // fireEvent.change(instructorSelect, { target: { value: "2" } });
+    // expect(screen.getByDisplayValue("bukky")).toBeInTheDocument();
 
     // ตรวจสอบว่าฟังก์ชัน fetchProduct ถูกเรียกเมื่อเปลี่ยน Filter
-    expect(mockAxiosGet).toHaveBeenCalledTimes(4); // หรือจำนวนครั้งที่คาดว่าจะเรียก
+    expect(mockAxiosGet).toHaveBeenCalledTimes(5); // หรือจำนวนครั้งที่คาดว่าจะเรียก
+
+    // Check parameters sent in the API call after changing filter
+    // ตรวจสอบว่า axios.get ถูกเรียกด้วย parameters ที่ถูกต้อง
+    expect(mockAxiosGet).toHaveBeenCalledWith(
+      "http://localhost:4000/api/getcourse",
+      expect.objectContaining({
+        params: expect.objectContaining({
+          page: 1,
+          limit: 9,
+          // Instructor: "2",
+          Status: "Open",
+          Level: "Beginner",
+          Sort: "Z-A",
+        }),
+      })
+    );
   });
 });
